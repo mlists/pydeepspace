d01848125fa57425a618e05045f80a2cac258776
michael <michael.kennedy@thedropbears.org.au>
2019-03-24 00:11:08 +1100

    Autonomous: moved acceleration to chassis
    Update some coordiantes

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -1,10 +1,16 @@
 from dataclasses import dataclass
 import math
 
-from magicbot.state_machine import AutonomousStateMachine, state
+from magicbot.state_machine import AutonomousStateMachine, state, timed_state
 import wpilib
 
-from automations.alignment import HatchDepositAligner, HatchIntakeAligner
+from automations.alignment import (
+    HatchDepositAligner,
+    HatchIntakeAligner,
+    CargoDepositAligner,
+)
+from automations.cargo import CargoManager
+from components.cargo import CargoManipulator
 from components.hatch import Hatch
 from components.vision import Vision
 from pyswervedrive.chassis import SwerveChassis
@@ -15,32 +21,51 @@ from utilities.pure_pursuit import PurePursuit, Waypoint, insert_trapezoidal_way
 @dataclass
 class Coordinates:
     start_pos: Waypoint
+    start_pos_cargo: Waypoint
     front_cargo_ship: Waypoint
     setup_loading_bay: Waypoint
     loading_bay: Waypoint
+    cargo_depot_setup: Waypoint
+    cargo_depot: Waypoint
     side_cargo_ship_alignment_point: Waypoint
+    side_cargo_ship_middle: Waypoint
     side_cargo_ship: Waypoint
+    cargo_endpoint: Waypoint
 
 
 left_coordinates = Coordinates(
     start_pos=Waypoint(
-        1.2 + SwerveChassis.LENGTH / 2, 0 + SwerveChassis.WIDTH / 2, 0, 2
+        1.3 + SwerveChassis.LENGTH / 2, 0 + SwerveChassis.WIDTH / 2, 0, 2
+    ),
+    start_pos_cargo=Waypoint(
+        1.3 + SwerveChassis.LENGTH / 2, 1.63 - SwerveChassis.WIDTH / 2, 0, 2
     ),
     front_cargo_ship=Waypoint(5.5 - SwerveChassis.LENGTH / 2, 0.3, 0, 1.5),
-    setup_loading_bay=Waypoint(3.3, 3, math.pi, 1),
-    loading_bay=Waypoint(0.2 + SwerveChassis.LENGTH / 2, 3.4, math.pi, 1),
+    setup_loading_bay=Waypoint(3.3, 3, math.pi, 2),
+    loading_bay=Waypoint(0.2 + SwerveChassis.LENGTH / 2, 3.4, math.pi, 1.5),
+    cargo_depot_setup=Waypoint(2.4, 3, -math.atan2(2.4 - 1.2, 3 - 2.3), 1),
+    cargo_depot=Waypoint(1.2, 2.3, -math.atan2(2.4 - 1.2, 3 - 2.3), 0.5),
     side_cargo_ship_alignment_point=Waypoint(
         6.6, 1.8 + SwerveChassis.WIDTH / 2, -math.pi / 2, 1.5
     ),
+    side_cargo_ship_middle=Waypoint(
+        (6.6 + 7.4) / 2, 0.8 + SwerveChassis.WIDTH / 2, math.pi / 2, 0.75
+    ),
     side_cargo_ship=Waypoint(6.6, 0.8 + SwerveChassis.WIDTH / 2, -math.pi / 2, 1),
+    cargo_endpoint=Waypoint(7.4, 1.3, 0, 3),
 )
 right_coordinates = Coordinates(
     start_pos=left_coordinates.start_pos.reflect(),
+    start_pos_cargo=left_coordinates.start_pos_cargo.reflect(),
     front_cargo_ship=left_coordinates.front_cargo_ship.reflect(),
     setup_loading_bay=left_coordinates.setup_loading_bay.reflect(),
     loading_bay=left_coordinates.loading_bay.reflect(),
+    cargo_depot_setup=left_coordinates.cargo_depot_setup.reflect(),
+    cargo_depot=left_coordinates.cargo_depot.reflect(),
     side_cargo_ship_alignment_point=left_coordinates.side_cargo_ship_alignment_point.reflect(),
+    side_cargo_ship_middle=left_coordinates.side_cargo_ship_middle.reflect(),
     side_cargo_ship=left_coordinates.side_cargo_ship.reflect(),
+    cargo_endpoint=left_coordinates.cargo_endpoint.reflect(),
 )
 
 
@@ -49,9 +74,11 @@ class AutoBase(AutonomousStateMachine):
     # Here magicbot injects components
     hatch_deposit: HatchDepositAligner
     hatch_intake: HatchIntakeAligner
+    cargo: CargoManager
 
     chassis: SwerveChassis
     hatch: Hatch
+    cargo_component: CargoManipulator
     imu: NavX
     vision: Vision
 
@@ -67,15 +94,11 @@ class AutoBase(AutonomousStateMachine):
         self.desired_angle_navx = 0
         self.minimum_path_completion = 0.85
 
-        self.acceleration = 2
-        self.deceleration = -0.25
-
         self.pursuit = PurePursuit(look_ahead=0.2, look_ahead_speed_modifier=0.25)
 
     def setup(self):
         self.hatch.has_hatch = True
-        self.hatch_intake.alignment_speed = 0.75
-        self.hatch_deposit.alignment_speed = 0.75
+        self.vision.camera = 0
 
     def on_enable(self):
         super().on_enable()
@@ -83,14 +106,23 @@ class AutoBase(AutonomousStateMachine):
         self.chassis.odometry_y = self.coordinates.start_pos.y
         self.completed_runs = 0
 
+    # @state(first=True)
+    # def intake_starting_hatch(self, initial_call):
+    #     if initial_call:
+    #         self.counter = 0
+    #     if self.counter > 5:
+    #         self.next_state("drive_to_cargo_ship")
+    #     else:
+    #         self.counter += 1
+
     @state(first=True)
     def drive_to_cargo_ship(self, initial_call):
         if initial_call:
             if self.completed_runs == 0:
                 waypoints = insert_trapezoidal_waypoints(
                     (self.current_pos, self.coordinates.front_cargo_ship),
-                    self.acceleration,
-                    self.deceleration,
+                    self.chassis.acceleration,
+                    self.chassis.deceleration,
                 )
             elif self.completed_runs == 1:
                 waypoints = insert_trapezoidal_waypoints(
@@ -99,8 +131,8 @@ class AutoBase(AutonomousStateMachine):
                         self.coordinates.side_cargo_ship_alignment_point,
                         self.coordinates.side_cargo_ship,
                     ),
-                    self.acceleration,
-                    self.deceleration,
+                    self.chassis.acceleration,
+                    self.chassis.deceleration,
                 )
             else:
                 self.next_state("drive_to_loading_bay")
@@ -137,8 +169,8 @@ class AutoBase(AutonomousStateMachine):
                         self.coordinates.setup_loading_bay,
                         self.coordinates.loading_bay,
                     ),
-                    self.acceleration,
-                    self.deceleration,
+                    self.chassis.acceleration,
+                    self.chassis.deceleration,
                 )
             elif self.completed_runs == 2:
                 waypoints = insert_trapezoidal_waypoints(
@@ -147,8 +179,8 @@ class AutoBase(AutonomousStateMachine):
                         self.coordinates.setup_loading_bay,
                         self.coordinates.loading_bay,
                     ),
-                    self.acceleration,
-                    self.deceleration,
+                    self.chassis.acceleration,
+                    self.chassis.deceleration,
                 )
             else:
                 self.next_state("stop")
@@ -236,8 +268,8 @@ class SideOnlyBase(AutoBase):
                     self.coordinates.side_cargo_ship_alignment_point,
                     self.coordinates.side_cargo_ship,
                 ),
-                self.acceleration,
-                self.deceleration,
+                self.chassis.acceleration,
+                self.chassis.deceleration,
             )
             self.pursuit.build_path(waypoints)
         self.follow_path()
@@ -271,15 +303,14 @@ class DriveForwards(AutonomousStateMachine):
 
     chassis: SwerveChassis
     imu: NavX
+    hatch: Hatch
+    cargo_component: CargoManipulator
 
     joystick: wpilib.Joystick
 
     def __init__(self):
         super().__init__()
         self.pursuit = PurePursuit(look_ahead=0.2, look_ahead_speed_modifier=0.25)
-
-        self.acceleration = 1
-        self.deceleration = -0.5
 
     def on_enable(self):
         super().on_enable()
@@ -288,6 +319,8 @@ class DriveForwards(AutonomousStateMachine):
 
     @state(first=True)
     def wait_for_input(self):
+        self.hatch.has_hatch = False
+        self.cargo_component.has_cargo = True
         if self.joystick.getY() < -0.5:  # joystick -y is forwards
             self.next_state("drive_forwards")
 
@@ -296,8 +329,8 @@ class DriveForwards(AutonomousStateMachine):
         if initial_call:
             waypoints = insert_trapezoidal_waypoints(
                 (self.current_pos, Waypoint(1.5, 0, 0, 0)),
-                acceleration=self.acceleration,
-                deceleration=self.deceleration,
+                acceleration=self.chassis.acceleration,
+                deceleration=self.chassis.deceleration,
             )
             self.pursuit.build_path(waypoints)
         self.follow_path()
@@ -326,8 +359,8 @@ class DoubleFrontBase(AutoBase):
             if self.completed_runs == 0:
                 waypoints = insert_trapezoidal_waypoints(
                     (self.current_pos, self.coordinates.front_cargo_ship),
-                    self.acceleration,
-                    self.deceleration,
+                    self.chassis.acceleration,
+                    self.chassis.deceleration,
                 )
             elif self.completed_runs == 1:
                 waypoints = insert_trapezoidal_waypoints(
@@ -336,8 +369,8 @@ class DoubleFrontBase(AutoBase):
                         self.coordinates.setup_loading_bay,
                         self.coordinates.front_cargo_ship.reflect(),
                     ),
-                    self.acceleration,
-                    self.deceleration,
+                    self.chassis.acceleration,
+                    self.chassis.deceleration,
                 )
             else:
                 self.next_state("drive_to_loading_bay")
@@ -355,14 +388,3 @@ class DoubleFrontBase(AutoBase):
 class LeftDoubleFront(DoubleFrontBase):
     MODE_NAME = "Left Double Front Hatch"
 
-    def __init__(self):
-        super().__init__()
-        self.coordinates = left_coordinates
-
-
-class RightDoubleFront(DoubleFrontBase):
-    MODE_NAME = "Right Double Front Hatch"
-
-    def __init__(self):
-        super().__init__()
-        self.coordinates = right_coordinates
